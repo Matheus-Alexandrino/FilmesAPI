2e5c6ca9d2f334c7d4594c79c77f56a2bc9eae87
Matheus-Alexandrino <matwsalmeida@gmail.com>
2025-07-18 21:32:37 -0300

    Add project files.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,25 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme) //Anotacao FromBody explicita que vem do body da req
+    {   
+        //if (!string.IsNullOrEmpty(filme.Titulo) &&
+        //    filme.Titulo.Length <= 200)
+        //    !string.IsNullOrEmpty(filme.Genero) &&
+        //    filme.Duracao >= 70)
+        //{ 
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+            Console.WriteLine(filme.Genero);
+        //}
+    }    
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Required(ErrorMessage = "O titulo do filme e obrigatorio")]
+    [MaxLength(20, ErrorMessage = "O titulo nao pode exceder 20 caracteres")]
+    public string Titulo { get; set; }
+    
+    [Required(ErrorMessage = "O genero do filme e obrigatorio")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duracao do filme e obrigatoria")]
+    public int Duracao { get; set; }
+    
+}
+
